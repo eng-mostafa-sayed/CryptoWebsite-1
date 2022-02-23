06973c37490e41cbcaeef666ef0714927f6163ab
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2022-02-23 07:35:41 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,6 @@ import { TimelineChartComponent } from './dashboard/timeline-chart/timeline-char
 
 import { ChartModule } from 'primeng/chart';
 import { ChoosePlanComponent } from './dashboard/plans/choose-plan/choose-plan.component';
-import { MonthComponent } from './dashboard/plans/month/month.component';
 import { Cperiod1Component } from './dashboard/plans/cperiod1/cperiod1.component';
 import { Cperiod2Component } from './dashboard/plans/cperiod2/cperiod2.component';
 import { Cperiod3Component } from './dashboard/plans/cperiod3/cperiod3.component';
@@ -73,7 +72,6 @@ import { Cperiod3Component } from './dashboard/plans/cperiod3/cperiod3.component
     TabsComponent,
     TimelineChartComponent,
     ChoosePlanComponent,
-    MonthComponent,
     Cperiod1Component,
     Cperiod2Component,
     Cperiod3Component,
