7934b3b84e2ad544794d78e9825cd15ae405a872
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2022-02-26 06:46:52 +0200

    updated plans

--- src/app/dashboard/plans/choose-plan/choose-plan.component.ts
+++ src/app/dashboard/plans/choose-plan/choose-plan.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoosePlanComponent implements OnInit {
   short = true;
+  plans = [
+    {
+      type: 'Lite Miners ',
+      icon: '',
+      crypto: 'BTC (Bitcoin)',
+      power: '',
+      pricePer: '',
+      profitability: '',
+      price: 200,
+    },
+    {
+      type: 'Regular Miners ',
+      icon: '',
+      crypto: 'BTC (Bitcoin)',
+      power: '',
+      pricePer: '',
+      profitability: '',
+      price: 500,
+    },
+    {
+      type: 'Pro Miners',
+      icon: '',
+      crypto: 'BTC (Bitcoin)',
+      power: '',
+      pricePer: '',
+      profitability: '',
+      price: 1000,
+    },
+    {
+      type: 'Elite Miners',
+      icon: '',
+      crypto: 'BTC (Bitcoin)',
+      power: '',
+      pricePer: '',
+      profitability: '',
+      price: 5000,
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
